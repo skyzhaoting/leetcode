f464e785dea9891dea5652817593a5cabaa04098
skyzhaoting <skyzhao89@qq.com>
2014-05-19 15:34:43 +0800

    LinkedListCycle.java

--- linklist/LinkedListCycle.java
+++ linklist/LinkedListCycle.java
@@ -12,13 +12,14 @@ public class LinkedListCycle
 			return false;
 		ListNode slow = head;
 		ListNode fast = head;
-		while(fast != null && fast.next.next != null)
+		while(true)
 		{
+			if(fast == null || fast.next == null)
+				return false;
 			slow = slow.next;
 			fast = fast.next.next;
 			if(slow == fast)
 				return true;
 		}
-		return false;
 	}
 }
